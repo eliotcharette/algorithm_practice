112ba6e10354ee56aaae0371f894dcff57a26de4
Eliot Charette <eliotcharette@gmail.com>
2018-10-15 13:16:24 -0700

    continuing along

--- completed_exercises/chunk/index.js
+++ completed_exercises/chunk/index.js
@@ -12,11 +12,10 @@ function chunk(array, size) {
   const chunked = [];
   let index = 0;
 
-  while (index < array.length) {
+  while(index < array.length){
     chunked.push(array.slice(index, index + size));
     index += size;
   }
-
   return chunked;
 }
 
--- completed_exercises/fizzbuzz/index.js
+++ completed_exercises/fizzbuzz/index.js
@@ -13,19 +13,35 @@
 //   buzz
 
 function fizzBuzz(n) {
-  for (let i = 1; i <= n; i++) {
-    // Is the number a multiple of 3 and 5?
-    if (i % 3 === 0 && i % 5 === 0) {
+  for(let i = 1; i <= n; i++){
+    if(i % 15 === 0){
       console.log('fizzbuzz');
-    } else if (i % 3 === 0) {
-      // Is the number a multiple of 3?
+    }
+    else if(i % 3 === 0){
       console.log('fizz');
-    } else if (i % 5 === 0) {
+    }
+    else if(i % 5 === 0){
       console.log('buzz');
     } else {
       console.log(i);
     }
   }
 }
+
+// function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     // Is the number a multiple of 3 and 5?
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('fizzbuzz');
+//     } else if (i % 3 === 0) {
+//       // Is the number a multiple of 3?
+//       console.log('fizz');
+//     } else if (i % 5 === 0) {
+//       console.log('buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
 
 module.exports = fizzBuzz;
